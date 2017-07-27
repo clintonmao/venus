34fa5ca21f4b1756265405e78dd66e5b8b70881b
zhangzhihua <zhangzhihua>
2017-07-27 18:24:06 +0800

    将基于XML配置服务相关类移到XML包中

---
+++ venus-client/src/main/java/com/meidusa/venus/client/VenusServiceFactory.java
@@ -0,0 +1,10 @@
+package com.meidusa.venus.client;
+
+import com.meidusa.venus.client.xml.XmlServiceFactory;
+
+/**
+ * 为了兼容原来配置，继承XmlServiceFactory
+ * Created by Zhangzhihua on 2017/7/27.
+ */
+public class VenusServiceFactory extends XmlServiceFactory{
+}
