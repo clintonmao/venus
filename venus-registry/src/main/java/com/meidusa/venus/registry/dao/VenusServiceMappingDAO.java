f79788f5201910367ba4553014cf94c357ab6b33
longhaisheng <longhaisheng@chexiang.com>
2017-08-03 16:29:21 +0800

    change registe method

--- venus-registry/src/main/java/com/meidusa/venus/registry/dao/VenusServiceMappingDAO.java
+++ venus-registry/src/main/java/com/meidusa/venus/registry/dao/VenusServiceMappingDAO.java
@@ -10,15 +10,17 @@ public interface VenusServiceMappingDAO {
 	boolean addServiceMapping(VenusServiceMappingDO venusServiceMappingDO) throws DAOException;
 
 	boolean updateServiceMapping(VenusServiceMappingDO venusServiceMappingDO) throws DAOException;
-	
-	boolean deleteServiceMapping(int serverId, int serviceId,String version, String role) throws DAOException;
 
-	VenusServiceMappingDO getServiceMapping(Integer serverId,Integer serviceId, String role) throws DAOException;
-	
+	boolean updateServiceMappingHeartBeatTime(int serverId, int serviceId, String version, String role) throws DAOException;
+
+	boolean deleteServiceMapping(int serverId, int serviceId, String version, String role) throws DAOException;
+
+	VenusServiceMappingDO getServiceMapping(Integer serverId, Integer serviceId, String role) throws DAOException;
+
 	List<VenusServiceMappingDO> getServiceMapping(Integer serviceId, String role) throws DAOException;
-	
+
 	VenusServiceMappingDO getServiceMapping(Integer id) throws DAOException;
-	
+
 	List<VenusServiceMappingDO> getServiceMappings(Integer serverId) throws DAOException;
 
 }
