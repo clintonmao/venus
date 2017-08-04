56176b5e87ca5e5bab826be1d82dd18770a3a710
zhangzhihua <zhangzhihua>
2017-08-04 17:11:54 +0800

    拆分response/ok/notify调用

--- venus-backend/src/main/java/com/meidusa/venus/backend/invoker/VenusInvokerHandler.java
+++ venus-backend/src/main/java/com/meidusa/venus/backend/invoker/VenusInvokerHandler.java
@@ -6,7 +6,7 @@ import com.meidusa.toolkit.common.util.Tuple;
 import com.meidusa.toolkit.net.Connection;
 import com.meidusa.venus.annotations.ExceptionCode;
 import com.meidusa.venus.annotations.RemoteException;
-import com.meidusa.venus.backend.invoker.async.RemotingInvocationListener;
+import com.meidusa.venus.backend.invoker.callback.RemotingInvocationListener;
 import com.meidusa.venus.backend.invoker.sync.EndpointInvocation;
 import com.meidusa.venus.backend.support.RequestInfo;
 import com.meidusa.venus.backend.support.Response;
