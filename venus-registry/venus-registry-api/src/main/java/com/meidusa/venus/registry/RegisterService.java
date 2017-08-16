b68abba2ebd1fd562febc4c511b724407c2f8ce5
zhangzhihua <zhangzhihua>
2017-08-16 17:35:53 +0800

    refactor registry

---
+++ venus-registry/venus-registry-api/src/main/java/com/meidusa/venus/registry/RegisterService.java
@@ -0,0 +1,64 @@
+package com.meidusa.venus.registry;
+
+import com.meidusa.venus.URL;
+import com.meidusa.venus.service.registry.ServiceDefinition;
+
+/**
+ * Created by Zhangzhihua on 2017/8/16.
+ */
+public interface RegisterService {
+
+    /**
+     * 服务注册
+     * @param url
+     * @throws VenusRegisteException
+     */
+    void registe(URL url) throws VenusRegisteException;
+
+    /**
+     * 服务反注册
+     * @param url
+     * @throws VenusRegisteException
+     */
+    void unregiste(URL url) throws VenusRegisteException;
+
+    /**
+     * 服务订阅
+     * @param url
+     * @throws VenusRegisteException
+     */
+    void subscrible(URL url) throws VenusRegisteException;
+
+    /**
+     * 服务反订阅
+     * @param url
+     * @throws VenusRegisteException
+     */
+    void unsubscrible(URL url) throws VenusRegisteException;
+
+    /**
+     * 提供方、消费方心跳检测
+     * @throws VenusRegisteException
+     */
+    void heartbeat() throws VenusRegisteException;
+
+    /**
+     * 从本地查找服务定义
+     * @param url
+     * @return
+     * @throws VenusRegisteException
+     */
+    ServiceDefinition lookup(URL url) throws VenusRegisteException;
+
+    /**
+     * 加载服务定义到本地
+     * @throws VenusRegisteException
+     */
+    void load() throws VenusRegisteException;
+
+    /**
+     * 销毁，清理、释放相关资源
+     * @throws VenusRegisteException
+     */
+    void destroy() throws VenusRegisteException;
+}
