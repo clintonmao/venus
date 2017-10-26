f6f0606a6897bc2c444602b937bcccf6efa0b14e
longhaisheng <longhaisheng@chexiang.com>
2017-10-26 11:00:54 +0800

    edit iputil

--- venus-commons/venus-common-base/src/main/java/com/meidusa/venus/util/NetUtil.java
+++ venus-commons/venus-common-base/src/main/java/com/meidusa/venus/util/NetUtil.java
@@ -6,41 +6,38 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
- * Net util
- * Created by Zhangzhihua on 2017/8/17.
+ * Net util Created by Zhangzhihua on 2017/8/17.
  */
 public class NetUtil {
 
-    private static String localIp;//TODO localIp可能变化
+	private static String localIp;// TODO localIp可能变化
 
-    public static String getLocalIp(boolean isCache){
-        try {
-            if(isCache){
-                return getLocalIp();
-            }
-            InetAddress addr = InetAddress.getLocalHost();
-            String localIp =  addr.getHostAddress();
-            return localIp;
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public static String getLocalIp(boolean isCache) {
+		// return
 
-    /**
-     * 获取本机ip,获取local ip太耗时，改为周期性获取
-     * @return
-     */
-    public static String getLocalIp(){
-        try {
-            if(StringUtils.isNotEmpty(localIp)){
-                return localIp;
-            }
-            InetAddress addr = InetAddress.getLocalHost();
-            String newLocalIp =  addr.getHostAddress();
-            localIp = newLocalIp;
-            return localIp;
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		if (isCache) {
+			return getLocalIp();
+		}
+		// InetAddress addr = InetAddress.getLocalHost();
+		// String localIp = addr.getHostAddress();
+		String localIp = NetInterfaceManager.INSTANCE.getLocalHostAddress();
+		return localIp;
+
+	}
+
+	/**
+	 * 获取本机ip,获取local ip太耗时，改为周期性获取
+	 * 
+	 * @return
+	 */
+	public static String getLocalIp() {
+		if (StringUtils.isNotEmpty(localIp)) {
+			return localIp;
+		}
+		// InetAddress addr = InetAddress.getLocalHost();
+		// String newLocalIp = addr.getHostAddress();
+		String newLocalIp = NetInterfaceManager.INSTANCE.getLocalHostAddress();
+		localIp = newLocalIp;
+		return localIp;
+	}
 }
