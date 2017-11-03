cccdb5f3e1a657f130d6188d3d572a82e7b7e4f0
longhaisheng <longhaisheng@chexiang.com>
2017-11-03 13:45:44 +0800

    change test

---
+++ venus-demo/venus-demo-api/src/main/java/com/chexiang/venus/demo/provider/model/OrderDO.java
@@ -0,0 +1,27 @@
+package com.chexiang.venus.demo.provider.model;
+
+public class OrderDO {
+	
+	private String name;
+	
+	private byte[] bytes;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte[] getBytes() {
+		return bytes;
+	}
+
+	public void setBytes(byte[] bytes) {
+		this.bytes = bytes;
+	}
+	
+	
+
+}
