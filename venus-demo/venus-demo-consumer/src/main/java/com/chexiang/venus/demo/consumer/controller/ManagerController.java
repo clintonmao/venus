16dfde16542b600a5196330759abca2c10d8d670
zhangzhihua <zhangzhihua>
2017-12-05 09:36:23 +0800

    update pom

--- venus-demo/venus-demo-consumer/src/main/java/com/chexiang/venus/demo/consumer/controller/ManagerController.java
+++ venus-demo/venus-demo-consumer/src/main/java/com/chexiang/venus/demo/consumer/controller/ManagerController.java
@@ -1,13 +1,7 @@
 package com.chexiang.venus.demo.consumer.controller;
 
-import com.chexiang.venus.demo.provider.HelloService;
-import com.chexiang.venus.demo.provider.model.Hello;
-import com.meidusa.venus.Result;
-import com.meidusa.venus.notify.InvocationListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
